850fe0cc2560555b2a0701c434a7bb658be63567
Mohammad Ashour <ashour.mohammad@gmail.com>
2023-05-26 17:59:16 +0100

    refactor: change ar to ar-EG in Arabic plural rules resolution

--- i18n/plurals.ts
+++ i18n/plurals.ts
@@ -1,5 +1,5 @@
 export const arabicPlurals = (choice: number): number => {
-  const name = new Intl.PluralRules("ar").select(choice)
+  const name = new Intl.PluralRules("ar-EG").select(choice)
 
   return { zero: 0, one: 1, two: 2, few: 3, many: 4, other: 5 }[name]
 }
